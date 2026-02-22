6978f9012b15725d7e7cf3665b71606dae04a3db
Gaurav Rathore <gauravsinghrathorethakur@gmail.com>
2026-02-22 18:40:24 +0530

    Update SEIR_project_1.py for web scraping functionality
    Refactored code to include web scraping from PyPI and improved HTML parsing.

--- SEIR_project_1.py
+++ SEIR_project_1.py
@@ -17,16 +17,34 @@ print("Done")
 
 # # >>>>>>   parsing HTMLL from bs4
 
-# import bs4
-res = requests.get('https://outlier.ai/')
-res.raise_for_status()
-noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
-print(type(noStarchSoup))
-if noStarchSoup.title:
-    print(noStarchSoup.title.text)
-else:
-    print("no title for this website")
+# # import bs4
+# res = requests.get('https://outlier.ai/')
+# res.raise_for_status()
+# noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
+# print(type(noStarchSoup))
+# if noStarchSoup.title:
+#     print(noStarchSoup.title.text)
+# else:
+#     print("no title for this website")
 
 # .......
 
 
+import requests
+from bs4 import BeautifulSoup
+
+URL = " https://pypi.org/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results_body = soup.find('body')
+# for result in results_body :
+print(results_body.prettify())
+
+results_a_tag = results_body.find_all('a')
+# print(results_a_tag)
+
+for tag in results_a_tag:
+        link_URL = tag.get("href")
+        print(link_URL)
