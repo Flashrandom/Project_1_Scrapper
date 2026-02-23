db3bfa17ccb8042d6a860a4d7e3368eded4979d0
Gaurav Rathore <gauravsinghrathorethakur@gmail.com>
2026-02-23 12:40:09 +0530

    Refactor web scraping into Crawler_Engine class
    Refactored code into a Crawler_Engine class to encapsulate crawling functionality and improved output handling.

--- SEIR_project_1.py
+++ SEIR_project_1.py
@@ -1,50 +1,30 @@
-import webbrowser
+
 import requests
 import bs4
 
-print("Opening browser...")
-# webbrowser.open('https://inventwithpython.com/')
-print("Done")
-
-
-# res = requests.get('https://outlier.ai/')
-# print(type(res))
-# print(res.status_code == requests.codes.ok)
-# print(len(res.text))
-
-# print(res.text[:])
-
-
-# # >>>>>>   parsing HTMLL from bs4
-
-# # import bs4
-# res = requests.get('https://outlier.ai/')
-# res.raise_for_status()
-# noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
-# print(type(noStarchSoup))
-# if noStarchSoup.title:
-#     print(noStarchSoup.title.text)
-# else:
-#     print("no title for this website")
-
-# .......
-
-
-import requests
 from bs4 import BeautifulSoup
+# Source - https://stackoverflow.com/a/4033743
 
-URL = " https://pypi.org/"
-page = requests.get(URL)
+class Crawler_Engine:
+        def __init__(self,URL_link):
+                self.URL_link = URL_link
+                      
+        def crawler(self):
+                # URL = "https://outlier.ai/"
+                page = requests.get(self.URL_link)
+                soup = BeautifulSoup(page.content, "html.parser")
+                
+                results_body = soup.find('body')
+                print(results_body.text,end="\n")
 
-soup = BeautifulSoup(page.content, "html.parser")
+                results_a_tag = results_body.find_all('a')
 
-results_body = soup.find('body')
-# for result in results_body :
-print(results_body.prettify())
+                for tag in results_a_tag:
+                        link_URL = tag.get("href")
+                        print(link_URL)
 
-results_a_tag = results_body.find_all('a')
-# print(results_a_tag)
+# Source - https://stackoverflow.com/a/70833
 
-for tag in results_a_tag:
-        link_URL = tag.get("href")
-        print(link_URL)
+import sys
+obj = Crawler_Engine(sys.argv[1])
+print(obj.crawler())
